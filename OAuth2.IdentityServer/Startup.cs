62533dc119828ec1607247d5e2a16f2e9a0324f6
Nhat Phi <phixuannhat@gmail.com>
2018-11-11 23:06:36 -0600

    remove unused codes

--- OAuth2.IdentityServer/Startup.cs
+++ OAuth2.IdentityServer/Startup.cs
@@ -34,11 +34,6 @@ namespace OAuth2.IdentityServer
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
             app.UseIdentityServer();
         }
     }
