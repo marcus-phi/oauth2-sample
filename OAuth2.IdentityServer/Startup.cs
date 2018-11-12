0dbb215b5495e746af61332fe223d0571c0a1dc8
Nhat Phi <phixuannhat@gmail.com>
2018-11-11 23:37:25 -0600

    constant issuer name

--- OAuth2.IdentityServer/Startup.cs
+++ OAuth2.IdentityServer/Startup.cs
@@ -25,7 +25,10 @@ namespace OAuth2.IdentityServer
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddIdentityServer()
+            services.AddIdentityServer(options =>
+                    {
+                        options.IssuerUri = "id-server";
+                    })
                     .AddDeveloperSigningCredential()
                     .AddInMemoryApiResources(Config.GetApiResources())
                     .AddInMemoryClients(Config.GetClients());
